016c8815e56b20291f152ed19dcc1f66a1f86574
lhegdal <lhegdal@anubis.in.conduct.no>
2016-07-07 13:12:39 +0200

    Added download jira-software

---
+++ manifests/download.pp
@@ -0,0 +1,17 @@
+class jira::download(
+  $path     = https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.1.9-x64.bin,
+  $version  = atlassian-jira-software-7.1.9-x64,
+  
+)
+{
+  exec { "download_jira":
+    command =>  "wget ${path}",
+    path    =>  "/usr/bin/wget",
+    creates =>  "/root/${version}.bin",
+    }
+   file {"/root/${version}.bin":
+    owner   =>  root,
+    group   =>  root,
+    mode    =>  755,
+    }
+}
